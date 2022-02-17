b9d5f488a55acb2c13d65c22ffbf8d86f6b5b249
Merryn <merrynhr@gmail.com>
2022-02-17 16:45:33 +0000

    created VideoList and VideoItem components to render search term  result titles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 
 class App extends React.Component {
+
+    state = { videos: [] }; 
+
     onTermSubmit = async term => {
       const response = await youtube.get('/search', {
         params: {
@@ -11,13 +15,14 @@ class App extends React.Component {
         }
       });
 
-      console.log(response);
+      this.setState({ videos: response.data.items })
     }
 
   render() {
     return ( 
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} /> 
       </div>
     )
   }
