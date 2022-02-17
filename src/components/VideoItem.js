6dfddc78168c68c96119f930fc208f178af4e518
Merryn <merrynhr@gmail.com>
2022-02-17 17:30:39 +0000

    created nested callback function, onVideoSelect, to select videos from rendered list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './VideoItem.css'
 
-const VideoItem = ({ video }) => {
-  return <div>{ video.snippet.title }</div>;
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">
+          { video.snippet.title }
+        </div>
+      </div>
+    </div>) 
 }
 
 export default VideoItem;
